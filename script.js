e00f0eec7e72f954cc198c943e868554c9911de0
Flegmon2001 <ruben.palkovits@gmail.com>
2026-03-26 12:52:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ function toBinary(char) {
 function convertText() {
     let input = document.getElementById("inputText").value;
 
-    // Split into words
     let words = input.split(" ");
 
     let result = words.map(word => {
-        // Convert each letter in word
         let letters = word.split("").map(letter => toBinary(letter));
         return letters.join(":");
     });
 
-    // Join words with "/"
     document.getElementById("outputText").value = result.join(" / ");
 }
